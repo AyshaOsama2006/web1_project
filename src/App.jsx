be6568e741c5452f5d02b59315d62cbf26fbe6a6
lujain <luj.muhtaseb@gmail.com>
2025-12-19 16:51:37 +0200

    Add Signup and Timer pages, update App.jsx with Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import './App.css';
-import Home from './components/Home.jsx'; 
+
+import Home from './components/Home.jsx';
 import Login from "./components/Login";
+import Signup from "./pages/Signup";
+import Timer from "./pages/Timer";
+
 function App() {
- 
-  return (
-    <div className="App">
-        <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-      
-      <Home />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/timer" element={<Timer />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
