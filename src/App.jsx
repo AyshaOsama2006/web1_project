8bb0277e003cdf98b9b89e3fd6f67baec1c41627
lujain <luj.muhtaseb@gmail.com>
2025-12-19 14:27:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home.jsx'; 
 import Login from "./components/Login";
+import Signup from "./pages/Signup";
+import Timer from "./pages/Timer";
 function App() {
- 
   return (
     <div className="App">
         <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-      </Routes>
+       <Route path="/signup" element={<Signup />} />
+          <Route path="/timer" element={<Timer />} />
+        </Routes>
     </Router>
-      
-      <Home />
     </div>
   );
 }
