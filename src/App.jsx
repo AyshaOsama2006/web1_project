031ba5772a785722ebb7f6a85dc3d747a758b3fe
AyshaOsama2006 <aysha_osama@icloud.com>
2025-12-21 22:15:48 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Login from "./components/Login.jsx";
 import Signup from "./components/signup.jsx";
 import Home from "./components/Home.jsx";
 import TodoList from "./components/TodoList.jsx";
-
 import Timer from "./components/Timer.jsx";
 import Profile from "./components/Profile.jsx";
-
 import StatisticsScreen from "./components/StatisticsScreen.jsx";
 import Upload from "./components/upload.jsx";
 
@@ -15,18 +13,17 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/todoList" element={<TodoList />} />
 
-      {}
+      <Route path="/todolist" element={<TodoList />} />
       <Route path="/timer" element={<Timer />} />
       <Route path="/profile" element={<Profile />} />
 
-      {}
       <Route path="/upload" element={<Upload />} />
-      <Route path="/StatisticsScreen" element={<StatisticsScreen />} />
+      <Route path="/statisticsscreen" element={<StatisticsScreen />} />
     </Routes>
   );
 }
